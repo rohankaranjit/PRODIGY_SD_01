f6d2078eb0d8dc2c9e256c27d8c941eb4db4cad9
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-12-20 23:16:22 +0545

    Update SD01.py

--- SD01.py
+++ SD01.py
@@ -28,3 +28,25 @@ def temperature_converter():
     # Input temperature value and unit from the user
     temperature = float(input("Enter the temperature value: "))
     unit = input("Enter the unit of measurement (Celsius, Fahrenheit, or Kelvin): ").lower()
+
+    # Based on the user input, perform the appropriate conversion and display the results
+    if unit == 'celsius':
+        celsius = temperature
+        fahrenheit = celsius_to_fahrenheit(celsius)
+        kelvin = celsius_to_kelvin(celsius)
+        print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit and {kelvin} Kelvin.")
+    elif unit == 'fahrenheit':
+        fahrenheit = temperature
+        celsius = fahrenheit_to_celsius(fahrenheit)
+        kelvin = fahrenheit_to_kelvin(fahrenheit)
+        print(f"{fahrenheit} degrees Fahrenheit is equal to {celsius} degrees Celsius and {kelvin} Kelvin.")
+    elif unit == 'kelvin':
+        kelvin = temperature
+        celsius = kelvin_to_celsius(kelvin)
+        fahrenheit = kelvin_to_fahrenheit(kelvin)
+        print(f"{kelvin} Kelvin is equal to {celsius} degrees Celsius and {fahrenheit} degrees Fahrenheit.")
+    else:
+        print("Invalid unit entered. Please enter Celsius, Fahrenheit, or Kelvin.")
+
+# Call the function to start the temperature conversion process
+temperature_converter()
