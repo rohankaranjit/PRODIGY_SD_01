d50e17035f919ee8004db411f50f98df21b78119
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-12-21 23:21:20 +0545

    Update SD01.py

--- SD01.py
+++ SD01.py
@@ -1,7 +1,6 @@
 # Function to convert Celsius to Fahrenheit
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
-
 # Function to convert Celsius to Kelvin
 def celsius_to_kelvin(celsius):
     return celsius + 273.15
