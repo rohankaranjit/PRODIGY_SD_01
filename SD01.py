38f27e320ccd4e21dbc4e607ae2e0f7c895f855b
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-12-21 23:21:13 +0545

    Update SD01.py

--- SD01.py
+++ SD01.py
@@ -10,7 +10,6 @@ def celsius_to_kelvin(celsius):
 def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
 
-
 # Function to convert Fahrenheit to Kelvin
 def fahrenheit_to_kelvin(fahrenheit):
     return (fahrenheit - 32) * 5/9 + 273.15
